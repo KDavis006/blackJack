5c38ef91d0b4e8387d6bd6db71a9169840c5f680
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-11 15:50:37 -0700

    made one of the dealers cards be face down so that the player does not have an advantage

--- public/game.js
+++ public/game.js
@@ -1,15 +1,19 @@
 const dealerCards = document.querySelector('.dealerContainer');
 const playerCards = document.querySelector('.playerContainer');
 
+const dealerBoard = []
+const playerBoard = []
+
 const startGame = async (req, res) => {
  const createShuffledDeck = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
-
+ console.log(createShuffledDeck)
  let deck_id = createShuffledDeck.data.deck_id
 
 
  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
  .then((res) => {
   const playerDraw = res.data.cards.map((x) => {
+   playerBoard.push({code: x.code, value: x.value})
   return `
   <img class="card" src="${x.image}" alt="${x.name}">
   `
@@ -19,14 +23,15 @@ const startGame = async (req, res) => {
 
  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
  .then((res) => {
-  console.log(res)
-  const dealerDraw = res.data.cards.map((x) => {
-  return `
-  <img class="card" src="${x.image}" alt="${x.name}">
+  console.log(res.data.cards)
+  dealerBoard.push({code: res.data.cards[0].code, value: res.data.cards[0].value})
+  dealerBoard.push({code: res.data.cards[1].code, value: res.data.cards[1].value})
+  dealerCards.innerHTML = `
+  <img class="card" src="${res.data.cards[0].image}" alt="${res.data.cards[0].value} of ${res.data.cards[0].suit}">
+  <img class="card" src="https://deckofcardsapi.com/static/img/back.png" alt="hidden card">
   `
   })
-  dealerCards.innerHTML = dealerDraw.join('')
-
- })
-}
+  
+ }
 startGame();
+
