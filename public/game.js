750dc515ebe1e759bfc11ebdd656697f76b99b97
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-12 12:58:45 -0700

    made the player hit function and added checks to see if i need to end the game

--- public/game.js
+++ public/game.js
@@ -4,11 +4,15 @@ const playerCards = document.querySelector('.playerContainer');
 const dealerBoard = []
 const playerBoard = []
 
-const startGame = async (req, res) => {
- const createShuffledDeck = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
- console.log(createShuffledDeck)
- let deck_id = createShuffledDeck.data.deck_id
+let createShuffledDeck;
 
+let deck_id;
+
+const startGame = async (req, res) => {
+
+ createShuffledDeck = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
+
+ deck_id = createShuffledDeck.data.deck_id
 
  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
  .then((res) => {
@@ -18,7 +22,7 @@ const startGame = async (req, res) => {
   <img class="card" src="${x.image}" alt="${x.name}">
   `
   })
-    playerCards.innerHTML = playerDraw.join('')
+    playerCards.innerHTML += playerDraw.join('')
  })
 
  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
@@ -31,7 +35,86 @@ const startGame = async (req, res) => {
   <img class="card" src="https://deckofcardsapi.com/static/img/back.png" alt="hidden card">
   `
   })
+
+  dealerBoard.map((x) => {
+    if (playerBoard[0].value == 'JACK' && playerBoard[1] == 'ACE'  || playerBoard[0].value == 'QUEEN' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'KING' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'KING' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'QUEEN' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'JACK') {
+      endGame()
+    }
+  })
+
+  playerBoard.map((x) => {
+    let cardValue = 0
+    if (playerBoard[0].value == 'JACK' && playerBoard[1] == 'ACE'  || playerBoard[0].value == 'QUEEN' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'KING' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'KING' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'QUEEN' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'JACK') {
+      endGame()
+    }
+    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
+      x.value = 10
+    } else if (x.value != 'ACE') {
+      x.value = parseInt(x.value)
+      cardValue += x.value
+    } else if (cardValue >= 11) {
+      x.value = 1
+      cardValue += x.value
+    } else {
+      x.value = 11
+      cardValue += x.value
+    } 
+    if (cardValue > 21) {
+      playerLose()
+    } else if (cardValue == 21) {
+      endGame()
+    } else {
+      return
+    }
+  })
   
  }
 startGame();
 
+
+
+async function playerHit() {
+  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+  .then((res) => {
+   playerBoard.push({code: res.data.cards[0].code, value: res.data.cards[0].value})
+   playerCards.innerHTML += `<img class="card" src="${res.data.cards[0].image}" alt="${res.data.cards[0].name}">`
+  })
+
+  playerBoard.map((x) => {
+    let cardValue = 0
+    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
+      x.value = 10
+    } else if (x.value != 'ACE') {
+      x.value = parseInt(x.value)
+      cardValue += x.value
+    } else if (cardValue >= 11) {
+      x.value = 1
+      cardValue += x.value
+    } else {
+      x.value = 11
+      cardValue += x.value
+    } 
+    if (cardValue > 21) {
+      playerLose()
+    } else {
+      return
+    }
+
+    if(playerBoard.length == 5) {
+      playerWin()
+    }
+
+  })
+  console.log(playerBoard)
+}
+
+async function dealerHit() {}
+
+
+async function endGame() {}
+
+
+async function playerWin() {}
+
+
+async function playerLose() {}
