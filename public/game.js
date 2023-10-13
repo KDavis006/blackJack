4156ec365976f3a14e5bd59f77cc2a233e038bf5
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-13 15:05:03 -0700

    trying to finish the functionality

--- public/game.js
+++ public/game.js
@@ -27,48 +27,36 @@ const startGame = async (req, res) => {
 
  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
  .then((res) => {
-  console.log(res.data.cards)
-  dealerBoard.push({code: res.data.cards[0].code, value: res.data.cards[0].value})
-  dealerBoard.push({code: res.data.cards[1].code, value: res.data.cards[1].value})
+  dealerBoard.push({code: res.data.cards[0].code, value: res.data.cards[0].value, image: res.data.cards[0].image})
+  dealerBoard.push({code: res.data.cards[1].code, value: res.data.cards[1].value, image: res.data.cards[1].image})
   dealerCards.innerHTML = `
   <img class="card" src="${res.data.cards[0].image}" alt="${res.data.cards[0].value} of ${res.data.cards[0].suit}">
   <img class="card" src="https://deckofcardsapi.com/static/img/back.png" alt="hidden card">
   `
   })
 
-  dealerBoard.map((x) => {
-    if (playerBoard[0].value == 'JACK' && playerBoard[1] == 'ACE'  || playerBoard[0].value == 'QUEEN' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'KING' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'KING' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'QUEEN' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'JACK') {
-      endGame()
-    }
-  })
+  const dealerCardValues = ['JACK', 'QUEEN', 'KING', 'ACE'];
+  const dealerHasFaceCardAndAce = (
+    (dealerCardValues.includes(dealerBoard[0].value) && dealerBoard[1].value === 'ACE') ||
+    (dealerCardValues.includes(dealerBoard[1].value) && dealerBoard[0].value === 'ACE')
+  );
 
-  playerBoard.map((x) => {
-    let cardValue = 0
-    if (playerBoard[0].value == 'JACK' && playerBoard[1] == 'ACE'  || playerBoard[0].value == 'QUEEN' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'KING' && playerBoard[1] == 'ACE' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'KING' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'QUEEN' || playerBoard[0].value == 'ACE' && playerBoard[1] == 'JACK') {
-      endGame()
-    }
-    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
-      x.value = 10
-    } else if (x.value != 'ACE') {
-      x.value = parseInt(x.value)
-      cardValue += x.value
-    } else if (cardValue >= 11) {
-      x.value = 1
-      cardValue += x.value
-    } else {
-      x.value = 11
-      cardValue += x.value
-    } 
-    if (cardValue > 21) {
-      playerLose()
-    } else if (cardValue == 21) {
-      endGame()
-    } else {
-      return
-    }
-  })
-  
- }
+  if (dealerHasFaceCardAndAce) {
+    endGame();
+    return
+  }
+
+  const playerCardValues = ['JACK', 'QUEEN', 'KING', 'ACE'];
+  const playerHasFaceCardAndAce = (
+    (playerCardValues.includes(playerBoard[0].value) && playerBoard[1].value === 'ACE') ||
+    (playerCardValues.includes(playerBoard[1].value) && playerBoard[0].value === 'ACE')
+  );
+
+  if (playerHasFaceCardAndAce) {
+    endGame();
+    return
+  }
+}
 startGame();
 
 
@@ -80,41 +68,176 @@ async function playerHit() {
    playerCards.innerHTML += `<img class="card" src="${res.data.cards[0].image}" alt="${res.data.cards[0].name}">`
   })
 
+  let cardValue = 0
   playerBoard.map((x) => {
-    let cardValue = 0
+
     if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
-      x.value = 10
+      cardValue += 10
     } else if (x.value != 'ACE') {
-      x.value = parseInt(x.value)
       cardValue += x.value
     } else if (cardValue >= 11) {
-      x.value = 1
-      cardValue += x.value
+      cardValue += 1
     } else {
-      x.value = 11
-      cardValue += x.value
+      cardValue += 11
     } 
-    if (cardValue > 21) {
+
+  })
+  if (cardValue > 21) {
       playerLose()
-    } else {
       return
     }
 
     if(playerBoard.length == 5) {
       playerWin()
+      return
     }
 
-  })
-  console.log(playerBoard)
 }
 
-async function dealerHit() {}
+async function dealerHit() {
+  await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+  .then((res) => {
+   dealerBoard.push({code: res.data.cards[0].code, value: res.data.cards[0].value})
+   dealerCards.innerHTML += `<img class="card" src="${res.data.cards[0].image}" alt="${res.data.cards[0].name}">`
+  })
+
+  let cardValue = 0
+  dealerBoard.map((x) => {
+    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
+      cardValue += 10
+    } else if (x.value != 'ACE') {
+      cardValue += parseInt(x.value)
+    } else if (cardValue >= 11) {
+      cardValue += 1
+    } else {
+      cardValue += 11
+    } 
+  })
+  if (cardValue > 21) {
+      playerWin()
+      return
+    }
+
+    if(dealerBoard.length == 5) {
+      playerLose()
+      return
+    }
+  endGame()
+}
 
 
-async function endGame() {}
+async function endGame() {
+  let playerValue = 0
+  let dealerValue = 0
+  if (dealerBoard.length == 2){
+    dealerCards.innerHTML = `
+    <img class="card" src="${dealerBoard[0].image}">
+    <img class="card" src="${dealerBoard[1].image}">`
+  }
+
+  playerBoard.map((x) => {
+      if (x.value == 'ACE'){
+        x.value = 11
+      }
+  })
+
+  console.log(playerBoard)
+
+  playerBoard.sort((a, b) => {
+    return a.value - b.value
+  })
+
+  console.log(playerBoard)
 
 
-async function playerWin() {}
+  playerBoard.map((x) => {
+    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
+      playerValue += 10
+    } else if (x.value == Number) {
+      playerValue += x.value
+    } else if (playerValue >= 11) {
+      x.value = 1
+      playerValue += 1
+    } else {
+      playerValue += 11
+    } 
+  })
+  
+  console.log(`Player Value: ${playerValue}`)
+
+  if (playerValue > 21) {
+      playerLose()
+      return
+    }
+
+  if(playerBoard.length == 5) {
+      playerWin()
+      return
+  }
+
+  dealerBoard.map((x) => {
+      if (x.value == 'ACE'){
+        x.value = 11
+      }
+  })
+
+  dealerBoard.sort((a, b) => {
+    return a.value - b.value
+  })
+
+  dealerBoard.map((x) => {
+    if (x.value == 'JACK' || x.value == 'QUEEN' || x.value == 'KING') {
+      x.value = 10
+    } else if (x.value != '11') {
+      x.value = parseInt(x.value)
+      dealerValue += x.value
+    } else if (dealerValue >= 11) {
+      x.value = 1
+      dealerValue += x.value
+    } else {
+      x.value = 11
+      dealerValue += x.value
+    } 
+  })
+  console.log(`Dealer Value: ${dealerValue}`)
+  if (dealerValue > 21) {
+    playerWin()
+    return
+  }
+
+  if(dealerBoard.length == 5) {
+    playerLose()
+    return
+  }
 
 
-async function playerLose() {}
+  if(dealerValue < 17) {
+    console.log('Dealer Hits')
+    dealerHit()
+  } else if (dealerValue < playerValue) {
+    playerWin()
+    return
+  } else if (dealerValue == playerValue) {
+    tie()
+    return
+  } else if (dealerValue > playerValue) {
+    playerLose()
+    return
+  }
+
+}
+
+
+async function playerWin() {
+  console.log('Player Wins')
+}
+
+
+async function playerLose() {
+  console.log('Player Lose')
+}
+
+
+async function tie() {
+  console.log("It's a Tie")
+}
